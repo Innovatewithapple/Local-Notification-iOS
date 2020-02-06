8450f647eac2194d770bf39448190911613d77a2
Mihir Vyas <mihirv126@gmail.com>
2020-02-06 18:34:32 +0530

    Add files via upload

---
+++ LocalNotification-master/LocalNotification/TableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TableViewController.swift
+//  LocalNotification
+//
+// //  Created by Mihir on 06.02.2019.
+//  Copyright © 2019 Mihir.Vyas. All rights reserved.
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let notifications = ["Local Notification",
+                         "Local Notification with Action",
+                         "Local Notification with Content",
+                         "Push Notification with  APNs",
+                         "Push Notification with Firebase",
+                         "Push Notification with Content"]
+    
+    var appDelegate = UIApplication.shared.delegate as? AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notifications.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.text = notifications[indexPath.row]
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let notificationType = notifications[indexPath.row]
+        
+        let alert = UIAlertController(title: "",
+                                      message: "After 5 seconds " + notificationType + " will appear",
+                                      preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            
+            self.appDelegate?.scheduleNotification(notificationType: notificationType)
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
